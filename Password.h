aa6f62d48006ce0321f7516b928a329364d3c702
twmccormic42@students.tntech.edu <twmccormic42@students.tntech.edu>
2017-02-02 15:19:30 -0600

    default comment

---
+++ Password.h
@@ -0,0 +1,21 @@
+#if !defined PASSWORD_H
+#define PASSWORD_H
+
+//complete the includes
+
+class Password
+{
+   private:
+      ListArray<String>* viable_words;  //the list of words that can still be the password
+      ListArray<String>* all_words;  //the original list of words
+      int len;  //the length of the first word entered is stored to check that all subsequent words have the same length
+
+      //a private helper method to report the number of character matches between two Strings
+      int getNumMatches(String* curr_word, String* word_guess);
+
+   public:
+
+
+};
+
+#endif
